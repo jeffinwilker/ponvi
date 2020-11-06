989ca92a7ac58173ea766eac38daa761c3a25e88
jeffinwilker <jeffersonwilker@outlook.com>
2020-11-05 23:49:30 -0300

    Config rotas 2
    Config de 2 rotas

--- src/App.js
+++ src/App.js
@@ -1,10 +1,7 @@
 import React from 'react';
+import Routes from './routes';
 
 function App() {
-    return (
-        <div>
-            <h1>teste</h1>
-        </div>
-    );
+    return <Routes />;
 }
 export default App;
---
+++ src/pages/Acompanhamento/index.js
@@ -0,0 +1,5 @@
+import React from 'react';
+
+export default function Acompanhamento() {
+    return <h1>teste acompanhamento</h1>;
+}
