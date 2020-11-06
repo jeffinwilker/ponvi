33c218bf7b9985241a5abe2a7701eeecba241851
jeffinwilker <47794451+jeffinwilker@users.noreply.github.com>
2020-11-06 14:09:52 -0300

    Criando inter
    Tá indo pouco a pouco.

---
+++ src/pages/Main/styles.js
@@ -0,0 +1,18 @@
+import styled from 'styled-components';
+
+export const Container = styled.h1`
+    color: #333;
+    max-width: 95%;
+    background: #fff;
+    border-radius: 40px;
+    box-shadow: 0 0 20px rgba(0, 0, 0, 0.1);
+    padding: 20px;
+    margin: 30px auto;
+`;
+
+export const ContainerInfo = styled.h1`
+    max-width: 75%;
+    background: #eff0f7;
+    border-radius: 40px;
+    padding: 20px;
+`;
---
+++ src/styles/global.js
@@ -0,0 +1,28 @@
+import { createGlobalStyle } from 'styled-components';
+
+export default createGlobalStyle`
+    * {
+        margin: 0;
+        padding: 0;
+        outline: 0;
+        box-sizing: border-box;
+    }
+
+    html, body, #root {
+        min-height: 100%;
+    }
+
+    body {
+        background: #E3E6E8;
+    }
+
+    body, input, button {
+        color: #5C35F7;
+        font-size: 6px;
+        font-family: -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Oxygen, Ubuntu, Cantarell, 'Open Sans', 'Helvetica Neue', sans-serif;
+    }
+
+    button {
+        cursor: pointer;
+    }
+    `;
