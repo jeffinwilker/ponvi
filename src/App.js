27a195f5d6a1b739b4da1d7ad2086880c5d85039
Jefferson Wilker <jeffersonwilker@outlook.com>
2020-11-08 20:20:35 -0300

    ajuste de rota

--- src/App.js
+++ src/App.js
@@ -1,15 +1,13 @@
 import React from 'react';
 import Routes from './routes';
-import GlobalStyle from './styles/global';
 
-import Routes from './routes'
 import {Container} from './style'
 
 function App() {
     return (
         <>
             <Container>
-                <Routes/>   
+                <Routes/>
             </Container>
         </>
     );
