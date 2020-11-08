118697efcaeeb1a414a0d9e4df7392767e7e8a7c
Fernando Igor <igor.aquiraz@gmail.com>
2020-11-08 15:28:46 -0300

    Ajustes no menu

--- src/style.js
+++ src/style.js
@@ -3,14 +3,15 @@ import styled from 'styled-components';
 export const Container = styled.div`
     color: #333;
     width: 90vw;
-    height: 90vh;
+    height: 83vh;
     background: #fff;
     border-radius: 30px;
     box-shadow: 0 0 20px rgba(0, 0, 0, 0.1);
     padding: 40px;
     margin: 30px auto;
     display: flex;
-    justify-content: start;
+    justify-content: flex-start;
+    align-items: center;
 `;
 
 export const Content = styled.div`
@@ -18,6 +19,6 @@ export const Content = styled.div`
     height: 100%;
     background: #eff0f7;
     border-radius: 26px;
-    padding:0;
+    padding: 40px 40px 0 40px;
     margin-left: 20px;
 `;
